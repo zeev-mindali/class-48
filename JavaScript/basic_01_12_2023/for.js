590f25bcc68705e5a8a53a9b05e94da892978afc
zeev mindali <mindalizeev@gmail.com>
2023-12-01 11:57:03 +0300

    js

--- JavaScript/basic_01_12_2023/for.js
+++ JavaScript/basic_01_12_2023/for.js
@@ -1,4 +1,12 @@
+/*
 var howManyTimes=10;
 for (var counter=0;counter<howManyTimes;counter++){
     console.log(counter+1,"I will never do it again !!!!!!");
+}
+*/
+var userInput = prompt ("how many numbers");
+var temp = 2;
+for (var counter=1;counter<=userInput;counter++){
+    console.log(temp);
+    temp *= 2;
 }
