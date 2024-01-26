7798a62d835764bc500ea262e4d3d799db1eef5f
zeev mindali <mindalizeev@gmail.com>
2024-01-26 13:15:51 +0300

    kartzya

---
+++ jquery/ajax_jquery_26_01_2024/mySwitchCase.js
@@ -0,0 +1,45 @@
+let day = "fri";
+
+// if (day === "sun") {
+//   console.log("יום ראשון דיכאון");
+// } else if (day === "mon") {
+//   console.log("יום שני עצבני");
+// } else if (day === "teu") {
+//   console.log("יום שלישי לא נגמר");
+// } else if (day === "wed") {
+//   console.log("יום רביעי מיותר");
+// } else if (day === "thu") {
+//   console.log("מצב רוח חופשי");
+// } else if (day === "fri") {
+//   console.log("אוי, כיתה 48");
+// } else if (day === "sat") {
+//   console.log("שבת קוייייידש");
+// } else {
+//   console.log("לא העיפרון הכי חד בקלמר");
+// }
+
+switch (day) {
+  case "sun":
+    console.log("יום ראשון דיכאון");
+    break;
+  case "mon":
+    console.log("יום שני עצבני");
+    break;
+  case "teu":
+    console.log("יום שלישי לא נגמר");
+    break;
+  case "wed":
+    console.log("יום רביעי מיותר");
+    break;
+  case "thu":
+    console.log("מצב רוח חופשי");
+    break;
+  case "fri":
+    console.log("אוי, כיתה 48");
+    break;
+  case "sat":
+    console.log("שבת קוייייידש");
+    break;
+  default:
+    console.log("לא העיפרון הכי חד בקלמר");
+}
