9a24212253f2367724d54eabb8ceaeb7f3fc3d84
zeev mindali <mindalizeev@gmail.com>
2024-03-08 11:12:16 +0200

    coins

--- react/coins/src/Components/Items/SingleCoin/SingleCoin.tsx
+++ react/coins/src/Components/Items/SingleCoin/SingleCoin.tsx
@@ -1,11 +1,21 @@
 import "./SingleCoin.css";
 
-function SingleCoin(): JSX.Element {
-    return (
-        <div className="SingleCoin Box">
-			
-        </div>
-    );
+interface CoinProps {
+  id: String;
+  symbol: String;
+  currencySymbol: String;
+  rateUsd: String;
+  type: String;
+}
+
+function SingleCoin(props: CoinProps): JSX.Element {
+  return <div className="SingleCoin Box">
+    <h2>{props.id}</h2><hr/>
+    <div>{props.symbol}</div>
+    <div>{props.currencySymbol}</div>
+    <div>{props.rateUsd}</div>
+    <div>{props.type}</div>
+  </div>;
 }
 
 export default SingleCoin;
--- react/coins/src/Components/Layout/CoinLayout/CoinLayout.tsx
+++ react/coins/src/Components/Layout/CoinLayout/CoinLayout.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import "./CoinLayout.css";
 import axios from "axios";
 import { CoinRate } from "../../model/CoinRate";
+import SingleCoin from "../../Items/SingleCoin/SingleCoin";
 
 function CoinLayout(): JSX.Element {
   //useState => will render the component again, once we will have a data....
@@ -23,7 +24,10 @@ function CoinLayout(): JSX.Element {
     });
   }, []);
 
-  return <div className="CoinLayout">main coin layout</div>;
+  return <div className="CoinLayout">
+    {coins.map(item=><SingleCoin id={item.id} symbol={item.symbol} currencySymbol={item.currencySymbol} 
+        rateUsd={item.rateUsd} type={item.type}/>)}
+  </div>;
 }
 
 export default CoinLayout;
