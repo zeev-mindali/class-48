8f439cb4d70b92d4d96d4f1d8ee159f2cec36637
zeev mindali <mindalizeev@gmail.com>
2024-03-22 11:13:18 +0200

    בשביל סמיון

--- react/cars/src/Components/Layout/MainMenu/MainMenu.tsx
+++ react/cars/src/Components/Layout/MainMenu/MainMenu.tsx
@@ -1,13 +1,14 @@
+import { NavLink } from "react-router-dom";
 import "./MainMenu.css";
 
 function MainMenu(): JSX.Element {
     return (
         <div className="MainMenu">
 			<h2>Main Menu</h2><hr/>
-            car locater<br/>
-            bike locater<br/>
-            truck locater<br/>
-            offride locater<br/>
+            <NavLink to={"/search/car"}>car locater</NavLink><br/>
+            <NavLink to={"/search/bike"}>bike locater</NavLink><br/>
+            <NavLink to={"/search/truck"}>truck locater</NavLink><br/>
+            <NavLink to={"/search/offroad"}>offride locater</NavLink><br/>
         </div>
     );
 }
--- react/cars/src/Components/Pages/SearchPage/SearchPage.tsx
+++ react/cars/src/Components/Pages/SearchPage/SearchPage.tsx
@@ -1,12 +1,19 @@
 import { useParams } from "react-router-dom";
 import "./SearchPage.css";
+import { useState } from "react";
 
 function SearchPage(): JSX.Element {
     //list of url from data.gov.il
+    const URL_CAR="";
+    const URL_BIKE="";
+    const URL_TRUCK="";
+    const URL_OFFROAD="";
 
+    const [myVtype,setVtype] = useState("");
     const params = useParams();
-    const vType = params.vechileType;
-
+    const vType = params.vechileType?.toLowerCase()||"car";
+    
+    setVtype(vType);
     const getData = ()=>{
         switch(vType){
             case "car":
@@ -28,6 +35,7 @@ function SearchPage(): JSX.Element {
         <div className="SearchPage">
 			Car locator<br/><hr/>
             <div className="Box">
+
                 <input type="text" /><input type="button" value={"search"}/>
             </div>
         </div>
