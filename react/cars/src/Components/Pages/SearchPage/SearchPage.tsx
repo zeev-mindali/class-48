6781c9c872a2627517a3075d6f376c07baaaaa31
zeev mindali <mindalizeev@gmail.com>
2024-03-22 12:37:26 +0200

    cars

--- react/cars/src/Components/Pages/SearchPage/SearchPage.tsx
+++ react/cars/src/Components/Pages/SearchPage/SearchPage.tsx
@@ -1,45 +1,67 @@
 import { useParams } from "react-router-dom";
 import "./SearchPage.css";
-import { useState } from "react";
+import { SyntheticEvent, useEffect, useState } from "react";
+import axios from "axios";
+import SingleItem from "../SingleItem/SingleItem";
+import { Car } from "../../model/Car";
 
 function SearchPage(): JSX.Element {
-    //list of url from data.gov.il
-    const URL_CAR="";
-    const URL_BIKE="";
-    const URL_TRUCK="";
-    const URL_OFFROAD="";
-
-    const [myVtype,setVtype] = useState("");
-    const params = useParams();
-    const vType = params.vechileType?.toLowerCase()||"car";
-    
-    setVtype(vType);
-    const getData = ()=>{
-        switch(vType){
-            case "car":
-
-            break;
-            case "bike":
-
-            break;
-            case "truck":
-
-            break;
-            case "offroad":
-
-            break;
-        }
+  //list of url from data.gov.il
+  const URL_CAR = "https://data.gov.il/api/3/action/datastore_search?resource_id=053cea08-09bc-40ec-8f7a-156f0677aff3&q=";
+  const URL_BIKE = "";
+  const URL_TRUCK = "";
+  const URL_OFFROAD = "";
+  const RESULT_LIMIT = 10;
+  const {vechileType} = useParams();
+  const [myVtype, setVtype] = useState(vechileType);
+  const [carData,setData] = useState("");
+  //useState setResult
+  const [carResult,setResult] = useState<Car[]>([]);
+  const getData = () => {
+    switch (myVtype) {
+      case "car":
+        break;
+      case "bike":
+        break;
+      case "truck":
+        break;
+      case "offroad":
+        break;
     }
+  };
 
-    return (
-        <div className="SearchPage">
-			Car locator<br/><hr/>
-            <div className="Box">
+  const handleTextChange = (args:SyntheticEvent)=>{
+    let mySearch = ((args.target as HTMLInputElement).value);
+    setData(mySearch);
+  }
+  const handleSearch = ()=>{
+    axios.get(URL_CAR+carData).then(res=>{
+        let myData:Car[] = [];
+        let myResponse = res.data.result.records;
+        for (let index=0;index<RESULT_LIMIT;index++){
+            myData.push(new Car(myResponse[index].baalut,myResponse[index].tozeret_nm,
+                myResponse[index].kinuy_mishari,myResponse[index].sug_delek_nm,
+                myResponse[index].mispar_rechev));
+            // if (index===10){
+            //     break;
+            // }
+        }
+        setResult(myData);
+    })
 
-                <input type="text" /><input type="button" value={"search"}/>
-            </div>
-        </div>
-    );
+  }
+  return (
+    <div className="SearchPage">
+      {myVtype} locator
+      <br />
+      <hr />
+      <div className="Box">
+        <input type="text" onChange={handleTextChange}/>
+        <input type="button" value={"search"} onClick={handleSearch}/>
+      </div><hr/>
+      {carResult.map((item,index)=><SingleItem key={index} carItem={item}/>)}
+    </div>
+  );
 }
 
 export default SearchPage;
--- react/cars/src/Components/Pages/SingleItem/SingleItem.tsx
+++ react/cars/src/Components/Pages/SingleItem/SingleItem.tsx
@@ -1,9 +1,19 @@
+import { Car } from "../../model/Car";
 import "./SingleItem.css";
 
-function SingleItem(): JSX.Element {
+interface carProps{
+    carItem:Car;
+}
+function SingleItem(props:carProps): JSX.Element {
     return (
-        <div className="SingleItem">
-			
+        <div className="SingleItem Box">
+            <b><span className="carNumber">{props.carItem.mispar_rechev}</span></b><hr/>
+            <div className="carInfo">
+                {props.carItem.baalut}<br/>
+                {props.carItem.tozeret_nm}<br/>
+                {props.carItem.kinuy_mishari}<br/>
+                {props.carItem.sug_delek_nm}<br/>
+            </div>
         </div>
     );
 }
