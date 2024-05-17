71fc467f851302b78a49a3207804a7bd9de4e990
zeev mindali <mindalizeev@gmail.com>
2024-05-17 11:00:54 +0300

    login and register with backend

--- react/cars/src/Components/Pages/register/register.tsx
+++ react/cars/src/Components/Pages/register/register.tsx
@@ -2,6 +2,7 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import "./register.css";
 import notify from "../../utils/Notify";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 type userCred = {
     userName:string,
@@ -11,6 +12,7 @@ type userCred = {
     userEmail:string,
 }
 function Register(): JSX.Element {
+    const navigate=useNavigate();
     const {register,handleSubmit, formState:{errors}} = useForm<userCred>();
 
     const makeLogin:SubmitHandler<userCred> = (data)=>{
@@ -18,27 +20,38 @@ function Register(): JSX.Element {
             notify.error("Password mismatch");
             return;
         }
-        //go to axios and send the information....
-        axios.post("/api/v1/login/registerUser")
-        .then (res=>{
 
+        let sendData = {
+            userPass:data.userPass,
+            userRole:data.userRole,
+            userEmail:data.userEmail,
+        }
+        //go to axios and send the information....
+        axios.post("http://localhost:8080/api/v1/login/registerUser",sendData)
+        .then (res=>{
+            notify.success("User was registered successfully");
+            navigate("/");
         })
         .catch(err=>{
-
+            notify.error("Error accord while registering the user");
+            console.log(err);
         })
     }
 
     const fieldNeed = {
         required:true,
         minLength:5,
-        maxLength:10
+        maxLength:15
     }
     return (
         <div className="register">
 			<div className="Box">
                 <h1>Reigster user</h1><hr/>
                 <form onSubmit={handleSubmit(makeLogin)}>
-                    <input type="text" placeholder="user name" {...register("userName",fieldNeed)}/><br/><br/>
+                    <input type="text" placeholder="user name" {...register("userName",fieldNeed)}/>
+                    {errors.userName?.type==="required" && <><br/><span style={{color:"red"}}>you must write user name</span></>}
+                    {errors.userName?.type==="minLength" && <><br/><span style={{color:"red"}}>user name must be 5 char minimum</span></>}
+                    {errors.userName?.type==="maxLength" && <><br/><span style={{color:"red"}}>user name must be 15 char maximum</span></>}<br/><br/>
                     <input type="password" placeholder="user password" {...register("userPass",fieldNeed)}/><br/><br/>
                     <input type="password" placeholder="check password"{...register("passCheck")}/><br/><br/>
                     <input type="text" placeholder="user email" {...register("userEmail")}/><br/><br/>
