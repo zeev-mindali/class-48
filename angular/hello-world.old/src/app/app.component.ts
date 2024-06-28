7bdb675bee3fd54a64c27c2e6085d53f5af91b26
zeevik the fox <mindalizeev@gmail.com>
2024-06-28 12:18:09 +0300

    angular

---
+++ angular/hello-world.old/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CLASS 48';
+  slogan = 'the best that lecturer can get';
+}
---
+++ angular/hello-world/src/app/app.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './mainLayout/header/header.component';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,HeaderComponent,FormsModule], //telling angular, which components we shell use....
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+    classNumber = "Class 48";
+    classSlogan = "The best that lecturer can get"
+
+    bossName = "Ofir";
+    bossComputer = "macbook air"; //little boss
+
+    userPlaceHolder = "tell me how do you feel";
+
+    userResponse = "";
+
+    userAge=0;
+
+    showWhoIsYourBoss(){
+        return `hello ${this.bossName} congrts for buying ${this.bossComputer}`;
+    }
+    // //primitive
+    // num:number = 5;
+    // num2:number = 4.3;
+    // num3:number = 0xff0000;
+
+    // word:string = "Hello";
+    // //כל מידע שנרצה להכניס, כי אנחנו לא החלטיים
+    // wordNum:any = "5";
+
+    // //מערכים
+
+    // //מערך שכל תא חייב להיות סטרינג
+    // word_ar:string[]  = ["jordan","dead sea","Meditarian"];
+    // //אפשרות נוספת להגדרת מערך של סטרינגים
+    // words2_arr:Array<string> = ["Ilan","Ben","Haim"];
+
+    // //מערך שנקבע לו את סוג התא, וכמות התאים
+    // //hybridAr:[string,number] = ["scrren_data_x",1024];
+    // //hybridAr:["scrren_data_x",1024] = "255,0,0,0";
+
+    // //פונקציה שחייב להחזיר מספר
+    // multi(x:number,y:number):number{
+    //     return x*y;
+    // }
+
+    onFeelingsUpdate(event:any){
+        console.log(event.target.value)
+        this.userResponse = event.target.value;
+    }
+}
