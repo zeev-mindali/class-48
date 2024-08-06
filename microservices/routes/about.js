e0a341b26d3b5fef6e0f0c35442007c82d718a62
zeev mindali <mindalizeev@gmail.com>
2024-08-06 19:30:27 +0300

    microservices 2

---
+++ microservices/routes/about.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const properties = require('../package.json');
+
+const aboutRoute = exporessRouter();
+
+aboutRoute.get("/", (req, res)=>{
+    const aboutInfo = {
+        name: properties.name,
+        description: properties.description,
+        author: properties.author
+    }
+    res.json(aboutInfo);
+});
+
+module.exports = aboutRoute;
